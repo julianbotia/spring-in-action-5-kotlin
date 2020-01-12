b5a61578b7d5971d0cce088c9d1a49373bcd6f88
Julian Botia <julianbotia@gmail.com>
2020-01-11 19:14:34 -0500

    first commit

---
+++ src/test/kotlin/com/brunosoft/springinaction/SpringInActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brunosoft.springinaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringInActionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
